7746ce1b8f9accd512c85c75bbc5f3dcd8e2ec22
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2024-02-14 02:38:52 +0530

    removed build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-import { Inter } from 'next/font/google'
-
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
 import { Metadata } from 'next'
 import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: "Chirag's Projects",
@@ -29,7 +26,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
-            <body className={`${inter.className} text-text`}>
+            <body className={`text-text`}>
                 <Navbar />
                 <main>{children}</main>
                 <Toaster />
