6d31f5ca270a5ce696f8c7fb3c3e7e8f257b223a
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2025-03-31 21:12:31 +0530

    added shuffle to projects display

--- app/codepens/page.tsx
+++ app/codepens/page.tsx
@@ -1,10 +1,11 @@
 import CodePensPage from "@/components/codePensPage";
 import api from "@/lib/api";
+import _ from "lodash";
 import React from "react";
 
 async function Page() {
   const projects = await api.projects.getProjects();
-  return <CodePensPage projects={projects} />;
+  return <CodePensPage projects={_.shuffle(projects)} />;
 }
 
 export default Page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import HomePage from "@/components/homePage";
 import api from "@/lib/api";
 import React from "react";
+import _ from "lodash";
 
 async function Page() {
   const projects = await api.projects.getProjects();
-  return <HomePage projects={projects} />;
+  return <HomePage projects={_.shuffle(projects)} />;
 }
 
 export default Page;
